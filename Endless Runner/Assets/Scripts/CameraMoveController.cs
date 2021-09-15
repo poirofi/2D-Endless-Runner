520bc064d7ea6aa4156a092ec068166924c487de
poirof <poirofirdaus@gmail.com>
2021-09-15 23:00:22 +0700

    EndlessRunner
    SI-AGATE

---
+++ Endless Runner/Assets/Scripts/CameraMoveController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveController : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [Header("Position")]
+    public Transform player;
+    public float horizontalOffset;
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.position.x + horizontalOffset;
+        transform.position = newPosition;
+    }
+}
